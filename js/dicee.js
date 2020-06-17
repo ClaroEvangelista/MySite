6e940914c00b3ee02073c7273312c327cb4ef139
ClaroEvangelista <jeprox999999@gmail.com>
2020-06-16 21:12:21 -0400

    Adding tindog page - one more change

---
+++ js/dicee.js
@@ -0,0 +1,12 @@
+var randonNumber1, randonNumber2;
+randonNumber1 = Math.floor((Math.random() * 6) + 1);
+document.querySelectorAll("img")[0].setAttribute("src", "images/diceeImages/dice" + randonNumber1 + ".png");
+randonNumber2 = Math.floor((Math.random() * 6) + 1);
+document.querySelectorAll("img")[1].setAttribute("src", "images/diceeImages/dice" + randonNumber2 + ".png");
+if (randonNumber1 > randonNumber2) {
+  document.querySelector("h1").innerText = "✨Player 1 wins!";
+} else if (randonNumber2 > randonNumber1){
+  document.querySelector("h1").innerText = "Player 2 wins!✨";
+} else {
+  document.querySelector("h1").innerText = "✨It's a tie!✨";
+}
